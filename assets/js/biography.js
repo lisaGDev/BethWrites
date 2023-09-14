a9acecc0f82d676cdff19d940e2d53a6ee4e0e31
Elizabeth Oviedo <elizoviedo@aol.com>
2023-09-14 18:28:03 -0400

    add biography page

--- assets/js/biography.js
+++ assets/js/biography.js
@@ -1,16 +1,3 @@
-const text = `Bienvenidos`;
-
-let i = 0;
-
-function writeText() {
-    document.querySelector('.musician').innerText = text.slice(0, i);
-    i++;
-    if (i > text.length) {
-        index = 0;
-    }
-}
-setInterval(writeText, 150);
-
 const toggler = document.querySelector('.navbar-toggler');
 var navbar = document.querySelector('.navbar');
 var prevScroll = window.pageYOffset;
